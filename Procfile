8606209090ae8b4d2fe00adc88c82adfaf9bd5a7
Joe Kutner <jpkutner@gmail.com>
2015-02-12 15:55:47 -0600

    first commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -jar target/dependency/jetty-runner.jar --port $PORT target/helloworld-1.0-SNAPSHOT.war
---
+++ src/main/java/Main.java
@@ -0,0 +1,22 @@
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+import java.nio.charset.Charset;
+
+public class Main extends HttpServlet {
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+      throws ServletException, IOException {
+    String output = "";
+
+    for (int i = 0; i < 1000; i++) {
+      output += "X";
+    }
+
+//    resp.addHeader("Content-length", String.valueOf(output.getBytes().length));
+    resp.addHeader("Connection", "keep-alive");
+    resp.addHeader("Content-Type", "text/plain");
+    resp.setContentLength(output.getBytes().length);
+    resp.getWriter().print(output);
+  }
+}
